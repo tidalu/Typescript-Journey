6e0e7132cc1729f4b0d273bcc7474e882cbcd531
Ulugbek Norbutaev <ul0271069@edu.uni.lodz.pl>
2023-12-26 22:38:48 +0100

    inference with conditional types

--- Intermadiate Typescript/index.ts
+++ Intermadiate Typescript/index.ts
@@ -296,3 +296,111 @@ type Exclude<T, U> = T extends U ? never : T
 
 // extract in the bg
 type Extract<T, U> = T extends U ? T : never
+
+
+
+
+// inference with conditional types
+
+// infer keyword is used to access sub part of type onformation within a larger type
+
+
+// infer keyword , which can only be used in the context of a condition expression (within conditional type declaration) is an imporetant tool for being able to extract out pience of type information from  other types
+
+
+
+class WebpackCompiler {
+  constructor(options: {
+    amd?: false | { [index: string]: any }
+    bail?: boolean
+    cache?:
+      | boolean
+      | {
+          maxGenerations?: number
+          type: "memory"
+        }
+
+    context?: string
+    dependencies?: string[]
+    devtool?: string | false
+    entry?: string
+    chunkLoading?: string | false
+
+    dependOn?: string | string[]
+    layer?: null | string
+    runtime?: string
+
+    wasmLoading?: string | false
+
+    externalsType?:
+      | "var"
+      | "module"
+      | "assign"
+      | "this"
+      | "window"
+      | "self"
+      | "global"
+      | "commonjs"
+      | "commonjs2"
+      | "commonjs-module"
+      | "amd"
+      | "amd-require"
+      | "umd"
+      | "umd2"
+      | "jsonp"
+      | "system"
+      | "promise"
+      | "import"
+      | "script"
+
+    ignoreWarnings?: (
+      | RegExp
+      | {
+          file?: RegExp
+
+          message?: RegExp
+
+          module?: RegExp
+        }
+    )[]
+
+    loader?: { [index: string]: any }
+    mode?: "development" | "production" | "none"
+    name?: string
+    parallelism?: number
+    profile?: boolean
+    recordsInputPath?: string | false
+    recordsOutputPath?: string | false
+    recordsPath?: string | false
+    stats?:
+      | boolean
+      | "none"
+      | "summary"
+      | "errors-only"
+      | "errors-warnings"
+      | "minimal"
+      | "normal"
+      | "detailed"
+      | "verbose"
+
+    target?: string | false | string[]
+
+    watch?: boolean
+  }) {}
+}
+/// ---cut---
+type ConstructorArg<C> = C extends {
+  new (arg: infer A, arg2: infer B, ...args: any[]): any
+}
+  ? [A, B]
+  : never
+
+class FruitStand {
+  constructor(fruitNames: string[], ali: number[]) {}
+}
+// our simple example
+let fruits: ConstructorArg<typeof FruitStand>
+//      ^?
+// our more realistic example
+let compilerOptions: ConstructorArg<typeof WebpackCompiler>
+//     ^?
