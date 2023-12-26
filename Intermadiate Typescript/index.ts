64ba090315653b35a928d712a1e7cdd7341aa681
Ulugbek Norbutaev <ul0271069@edu.uni.lodz.pl>
2023-12-26 20:52:30 +0100

    declaration merging

--- Intermadiate Typescript/index.ts
+++ Intermadiate Typescript/index.ts
@@ -1,1 +1,57 @@
-// whole 
+//
+
+// declaration merging 
+interface Fruit {
+    name: string
+    mass: number
+    color: string 
+}
+
+const Abanan: Fruit = {
+    name: "banana", 
+    color: "yellow", 
+    mass: 123
+}
+
+// we can stack multiple things on top of each other , like :
+
+interface Game {
+    players: number
+    place: string
+}
+
+const Game = {
+    players: 2, 
+    place: "open air"
+}
+
+export { Game }  // here it is two things satcked on top of each other  type alias, and a thing
+
+// there is actualy tihrd thing to stack on it
+
+
+
+class Anime {
+    static createBanana(): Anime {
+        return { name: "smth", color: "yellow", count: 123}
+    }
+}
+
+namespace Anime {
+    function createBanana(): Anime {
+        // the type 
+        return Anime.createBanana() // the class
+    }
+}
+
+interface Anime {
+    name: string
+    color: string 
+    count: number
+}
+
+export { Anime }
+// in the thing above , we have one identifier that is three thing in one,
+// a value (class)
+// a type
+// a namespace 
